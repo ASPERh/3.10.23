c9c35ab5149dc070b1205e5d45187c7ee80d5408
ASPER <144143767+ASPERh@users.noreply.github.com>
2023-11-23 23:32:08 +0000

    Add files via upload

---
+++ 3.10.23/3.10.23/3.10.23.cpp
@@ -0,0 +1,38 @@
+﻿
+//void SetCursor(int x, int y, Colors color, string text)
+//{
+//    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//    COORD pos;
+//    pos.X = x;
+//    pos.Y = y;
+//    SetConsoleCursorPosition(h, pos);
+//    SetConsoleTextAttribute(h, color);
+//    cout << text;
+//    cout << "\n";
+//}
+
+//void SetCursor(int x, int y) {
+//    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//    COORD pos;
+//    pos.X = x;
+//    pos.Y = y;
+//    SetConsoleCursorPosition(h, pos);
+//}
+
+//void SetCursor(int x, int y, Colors color, string text) {
+//    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//    COORD pos;
+//    pos.X = x;
+//    pos.Y = y;
+//    SetConsoleCursorPosition(h, pos);
+//    SetConsoleTextAttribute(h, color);
+//    cout << text;
+//    cout << "\n";
+//}
+
+//void SetCursor(string text) {
+//    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//    SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+//    cout << text;
+//    cout << "\n";
+//}
